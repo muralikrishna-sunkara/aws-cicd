7353921ef38f1fe13ebbaacb302be18423378dc2
Murali Sunkara <sunkaramurali1712@gmail.com>
2022-03-22 20:24:58 +0000

    commit

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -70,7 +70,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             output_artifacts = ["tf-code"]
             configuration = {
                 FullRepositoryId = "muralikrishna-sunkara/aws-cicd"
-                BranchName   = "master"
+                BranchName   = "main"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
             }
