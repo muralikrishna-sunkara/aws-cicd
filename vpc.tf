465b274ece4af8d2bd19a125d9f935121ca744c2
Murali Sunkara <sunkaramurali1712@gmail.com>
2022-03-23 09:53:30 +0000

    commit

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "default" {
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.default.id
-  cidr_block = "1.0.1.0/24"
+  cidr_block = "1.2.1.0/24"
 
   tags = {
     Name = "Main"
